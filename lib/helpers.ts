238ad3d2edaef63344a00a6f4ae0a0a8ce6184f8
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-06 13:09:45 -0400

    Created Helper PostData Fn to Post and Receive Response
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ lib/helpers.ts
@@ -0,0 +1,23 @@
+interface postDataProps {
+  url: string;
+  data?: any;
+}
+
+export const postData = async ({ url, data }: postDataProps) => {
+  console.log('POST REQUEST:', url, data);
+
+  const res: Response = await fetch(url, {
+    method: 'POST',
+    headers: new Headers({ 'Content-Type': 'application/json' }),
+    credentials: 'same-origin',
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    console.log('Error in POST', { url, data, res });
+
+    throw new Error(res.statusText);
+  }
+
+  return res.json();
+};

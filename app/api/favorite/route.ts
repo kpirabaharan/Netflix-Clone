e8b923abd1d9f738aaa3d5e152c8b8ebae11385e
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-10 17:10:03 -0400

    Added Favorite Button + POST and DELETE API for Like/DisLike
    Like works dislike doesnt
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ app/api/favorite/route.ts
@@ -0,0 +1,63 @@
+import { without } from 'lodash';
+import { NextResponse } from 'next/server';
+
+import prisma from '@/lib/prismadb';
+import serverAuth from '@/lib/serverAuth';
+
+export const POST = async (req: Request) => {
+  try {
+    const { currentUser } = await serverAuth();
+
+    const { movieId } = await req.json();
+
+    const existingMovie = await prisma.movie.findUnique({
+      where: { id: movieId },
+    });
+
+    if (!existingMovie) {
+      throw new Error('Invalid ID');
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { email: currentUser.email || '' },
+      data: { favoriteIds: { push: movieId } },
+    });
+
+    return NextResponse.json(updatedUser, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const { currentUser } = await serverAuth();
+
+    const request = await req.json();
+
+    console.log(request);
+
+    // const existingMovie = await prisma.movie.findUnique({
+    //   where: { id: movieId },
+    // });
+
+    // if (!existingMovie) {
+    //   throw new Error('Invalid ID');
+    // }
+
+    // const updatedFavoriteIds = without(currentUser.favoriteIds, movieId);
+
+    // const updatedUser = await prisma.user.update({
+    //   where: {
+    //     email: currentUser.email || '',
+    //   },
+    //   data: { favoriteIds: updatedFavoriteIds },
+    // });
+
+    // return NextResponse.json(updatedUser, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+};
---
+++ components/FavoriteButton.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import axios from 'axios';
+import { useCallback, useMemo } from 'react';
+import { AiOutlinePlus, AiOutlineCheck } from 'react-icons/ai';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useFavorites from '@/hooks/useFavorites';
+
+interface FavoriteButtonProps {
+  movieId: string;
+}
+
+const FavoriteButton = ({ movieId }: FavoriteButtonProps) => {
+  const { mutate: mutateFavorites } = useFavorites();
+  const { user: currentUser, mutate } = useCurrentUser();
+
+  const isFavorite = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(movieId);
+  }, [currentUser, movieId]);
+
+  const toggleFavorite = useCallback(async () => {
+    let response;
+
+    console.log({ isFavorite, movieId });
+
+    if (isFavorite) {
+      response = await axios.delete('/api/favorite', { data: movieId });
+    } else {
+      response = await axios.post('/api/favorite', { movieId });
+    }
+
+    const updatedFavoriteIds = response?.data?.favoriteIds;
+
+    mutate({
+      ...currentUser,
+      favoriteIds: updatedFavoriteIds,
+    });
+
+    mutateFavorites();
+  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
+
+  const Icon = isFavorite ? AiOutlineCheck : AiOutlinePlus;
+
+  return (
+    <div
+      className='cursor-pointer group/item w-6 h-6 lg:w-10 lg:h-10 rounded-full 
+      border-2 flex justify-center items-center transition border-neutral-400
+       hover:border-white'
+      onClick={toggleFavorite}
+    >
+      <Icon
+        className='text-neutral-400 transition group-hover/item:text-white'
+        size={25}
+      />
+    </div>
+  );
+};
+
+export default FavoriteButton;

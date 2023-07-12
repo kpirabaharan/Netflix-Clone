9158604de7dea20fa49aa386d7d2f023e3efb29e
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-12 13:29:09 -0400

    Dissappearing Navbar on Mouse Halt
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/watch/[movieId]/page.tsx
+++ app/watch/[movieId]/page.tsx
@@ -4,11 +4,13 @@ import { useParams, useRouter } from 'next/navigation';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 
 import { Movie } from '@/types';
+import useMousePosition from '@/hooks/useMousePosition';
 import useMovie from '@/hooks/useMovie';
 
 const Watch = () => {
   const { movieId } = useParams();
   const router = useRouter();
+  const mouseMoving = useMousePosition();
 
   const { movie }: { movie: Movie } = useMovie(movieId);
 
@@ -19,9 +21,9 @@ const Watch = () => {
   return (
     <div className='h-screen w-screen bg-black group'>
       <nav
-        className='fixed w-full p-4 z-10 flex flex-row items-center gap-8 
-        bg-black bg-opacity-70 opacity-0 transition 
-        duration-500 ease-in group-hover:opacity-70'
+        className={`fixed w-full p-4 z-10 flex flex-row items-center gap-8 
+        bg-black bg-opacity-70 transition duration-300 ease-in 
+        ${mouseMoving ? 'opacity-100' : 'opacity-0'}`}
       >
         <AiOutlineArrowLeft
           className='text-white cursor-pointer'
---
+++ hooks/useMousePosition.ts
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+interface MousePositionProps {
+  x: number | null;
+  y: number | null;
+}
+
+const useMousePosition = () => {
+  const [mousePosition, setMousePosition] = useState<MousePositionProps>({
+    x: null,
+    y: null,
+  });
+
+  const [mouseMoving, setMouseMoving] = useState(false);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    const updateMousePosition = (ev: MouseEvent) => {
+      setMousePosition({ x: ev.clientX, y: ev.clientY });
+
+      if (ev.clientX !== mousePosition.x || ev.clientY !== mousePosition.y) {
+        clearTimeout(timer);
+        setMouseMoving(true);
+        timer = setTimeout(() => {
+          setMouseMoving(false);
+        }, 2500);
+      }
+    };
+
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () => {
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
+  return mouseMoving;
+};
+export default useMousePosition;

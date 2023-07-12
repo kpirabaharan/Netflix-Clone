077945ff0b35e79a1c140c444fe4c65bf5eb8db6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-11 22:58:30 -0400

    Created Hook to Fetch Movie
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ hooks/useMovie.ts
@@ -0,0 +1,19 @@
+import useSWR from 'swr';
+
+import fetcher from '@/lib/fetcher';
+
+const useMovie = (id?: string) => {
+  const { data, error, isLoading } = useSWR(
+    id ? `/api/movies/${id}` : null,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    },
+  );
+
+  return { movie: data, error, isLoading };
+};
+
+export default useMovie;

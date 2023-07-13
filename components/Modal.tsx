979b032e758d8e74d3be3917abde296c15ad37f8
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-12 22:03:55 -0400

    Created Modal with RadixUI Dialog
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ components/Modal.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren } from 'react';
+import { Root, Portal, Overlay, Content, Close } from '@radix-ui/react-dialog';
+import { IoMdClose } from 'react-icons/io';
+
+interface ModalProps extends PropsWithChildren {
+  isOpen: boolean;
+  onChange: (open: boolean) => void;
+}
+
+const Modal = ({
+  isOpen,
+  onChange,
+
+  children,
+}: ModalProps) => {
+  return (
+    <Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
+      <Portal>
+        <Overlay className='bg-neutral-900/90 backdrop-blur-sm fixed inset-0' />
+        <Content
+          className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
+          w-[90%] md:w-[80%] mx-auto max-w-3xl bg-zinc-900'
+        >
+          <div>{children}</div>
+          <Close asChild>
+            <button
+              className='absolute top-3 right-3 inline-flex h-10 w-10 rounded-full
+              bg-black appearance-none items-center justify-center focus:outline-none
+              border hover:border-opacity-70 border-white transition duration
+              group/item'
+              onClick={() => {}}
+            >
+              <IoMdClose
+                size={25}
+                className='text-white transition duration group-hover/item:text-opacity-70'
+              />
+            </button>
+          </Close>
+        </Content>
+      </Portal>
+    </Root>
+  );
+};
+
+export default Modal;

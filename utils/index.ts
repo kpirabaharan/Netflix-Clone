0434824b0da258dacd8831ee8881b2fa7d3d22c6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-14 11:43:44 -0400

    Added Framer Motion Variant Utils
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ utils/index.ts
@@ -0,0 +1,103 @@
+interface VariantProps {
+  delay: number;
+  direction: string;
+  type: string;
+  duration: number;
+}
+
+export const textVariant = ({ delay }: VariantProps) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: 'spring',
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+
+export const fadeIn = ({ direction, type, delay, duration }: VariantProps) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+      y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+      opacity: 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
+
+export const zoomIn = ({ delay, duration }: VariantProps) => {
+  return {
+    hidden: {
+      scale: 0,
+      opacity: 0,
+    },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        type: 'tween',
+        delay: delay,
+        duration: duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
+
+export const slideIn = ({ direction, type, delay, duration }: VariantProps) => {
+  return {
+    hidden: {
+      x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+      y: direction === 'up' ? '100%' : direction === 'down' ? '-100%' : 0,
+    },
+    show: {
+      x: 0,
+      y: 0,
+      transition: {
+        type: type,
+        delay: delay,
+        duration: duration,
+        ease: 'easeOut',
+      },
+    },
+  };
+};
+
+interface StaggerContainerProps {
+  staggerChildren: any;
+  delayChildren: any;
+}
+
+export const staggerContainer = ({
+  staggerChildren,
+  delayChildren,
+}: StaggerContainerProps) => {
+  return {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: staggerChildren,
+        delayChildren: delayChildren || 0,
+      },
+    },
+  };
+};

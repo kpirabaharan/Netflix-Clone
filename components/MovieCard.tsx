6b479a14efdaa09714d0b5fc31127463430999ea
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-14 21:29:12 -0400

    Created Slide In Div for MovieCard
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
+import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { Movie } from '@/types';
 import useInfoModal from '@/hooks/useInfoModal';
+import useMediaQuery from '@/hooks/useMediaQuery';
 import { slideIn, fadeIn } from '@/utils';
 
 import FavoriteButton from './Buttons/FavoriteButton';
@@ -16,52 +18,77 @@ interface MovieCardProps {
 }
 
 const MovieCard = ({ movie }: MovieCardProps) => {
+  const isXSScreens = useMediaQuery('(max-width: 639px)');
+  const [isHovered, setIsHovered] = useState(false);
+
   const { onOpen } = useInfoModal();
+
+  const animate = isHovered ? 'open' : 'closed';
+
+  const variants = {
+    open: { opacity: 1, y: 0, scale: 1 },
+    closed: { opacity: 0, y: '100%', scale: 1 },
+  };
+
+  const transition = {
+    duration: 0.25,
+    delay: 0.1,
+  };
 
   return (
     <div
-      className='group bg-zinc-900 col-span relative h-[24vw] sm:h-[18vw] 
-      lg:h-[13vw] xl:h-[9vw] hover:scale-125 hover:z-50 transition duration-300'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={
+        isXSScreens
+          ? () => {
+              console.log('Woof');
+              onOpen(movie.id);
+            }
+          : () => {}
+      }
+      className='group col-span relative h-[25vw] md:h-[18vw] lg:h-[13vw] 2xl:h-[9vw] 
+      sm:hover:scale-125 hover:z-20 transition duration-300 cursor-pointer sm:cursor-default'
     >
       <img
         className='object-cover transition duration shadow-xl rounded-md 
-        group-hover:opacity-90 delay-300 w-full cursor-pointer h-full'
+        group-hover:opacity-90 w-full h-full'
         src={movie.thumbnailUrl}
         alt='Thumbnail'
       />
 
-      <motion.div
-        variants={slideIn('up', 'tween', 0.5, 2)}
-        className='absolute top-0 left-0 h-full w-full bg-gray-600/30 rounded-md
-          opacity-0 hover:opacity-100 transition duration-300'
+      {/* Hover Div */}
+      <div
+        className='hidden sm:flex flex-col justify-end px-2 pb-2 h-full w-full
+        absolute top-0 left-0 rounded-md'
       >
-        <div className='flex flex-col justify-end px-2 pb-2 h-full w-full'>
-          <div className='flex flex-row items-center justify-start gap-x-2 lg:gap-3'>
-            <PlayIcon movieId={movie.id} />
-            <FavoriteButton movieId={movie.id} />
-            <MoreIcon movieId={movie.id} onOpen={onOpen} />
-          </div>
-          <p className='text-white text-lg font-bold truncate'>{movie.title}</p>
-          <p className='text-white text-xs font-semibold truncate'>
-            {movie.genre}
-          </p>
-        </div>
-      </motion.div>
-
-      {/* <div className='z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md'>
-        
-        <p className='text-green-400 font-semibold mt-4'>
-          New <span className='text-white'>2023</span>
-        </p>
-
-        <div className='flex flex-row mt-4 gap-2 items-center'>
-          <p className='text-white text-[10px] lg:text-sm'>{movie.duration}</p>
-        </div>
-
-        <div className='flex flex-row mt-4 gap-2 items-center'>
-          <p className='text-white text-[10px] lg:text-sm'>{movie.genre}</p>
-        </div>
-      </div> */}
+        <motion.div
+          animate={animate}
+          variants={variants}
+          transition={transition}
+          className='flex flex-row items-center justify-start gap-x-2 lg:gap-3'
+        >
+          <PlayIcon movieId={movie.id} />
+          <FavoriteButton movieId={movie.id} />
+          <MoreIcon movieId={movie.id} onOpen={onOpen} />
+        </motion.div>
+        <motion.p
+          animate={animate}
+          variants={variants}
+          transition={transition}
+          className='text-white text-lg font-bold truncate'
+        >
+          {movie.title}
+        </motion.p>
+        <motion.p
+          animate={animate}
+          variants={variants}
+          transition={transition}
+          className='text-white text-xs font-semibold truncate'
+        >
+          {movie.genre}
+        </motion.p>
+      </div>
     </div>
   );
 };

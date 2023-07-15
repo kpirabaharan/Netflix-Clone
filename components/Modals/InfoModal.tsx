aa96562d32531e9e7c0b72616a28152a66f57edd
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-15 13:05:35 -0400

    Completed Mobile Modal
    Removed modal component as the two modals I'm using are too different to
    be modular
    
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/Modals/InfoModal.tsx
+++ components/Modals/InfoModal.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { Root, Portal, Overlay, Content, Close } from '@radix-ui/react-dialog';
+import { IoMdClose } from 'react-icons/io';
+
 import { Movie } from '@/types';
 import useInfoModal from '@/hooks/useInfoModal';
 import useMovie from '@/hooks/useMovie';
 
-import Modal from './Modal';
 import PlayButton from '../Buttons/PlayButton';
 import FavoriteButton from '../Buttons/FavoriteButton';
 
@@ -24,45 +26,68 @@ const InfoModal = () => {
   }
 
   return (
-    <Modal isOpen={isOpen} onChange={onChange}>
-      <div className='relative h-96'>
-        <video
-          className='w-full brightness-[70%] object-cover h-full'
-          src={movie.videoUrl}
-          poster={movie.thumbnailUrl}
-          autoPlay
-          loop
-          muted
-        />
-        <div className='absolute bottom-[10%] left-10'>
-          <div className='flex flex-row gap-4 items-center'>
-            <PlayButton movieId={movie.id} />
-            <FavoriteButton movieId={movie.id} />
+    <Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
+      <Portal>
+        <Overlay className='bg-neutral-900/90 backdrop-blur-sm fixed inset-0 z-30' />
+        <Content
+          className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
+          w-[90%] md:w-[80%] mx-auto max-w-3xl z-40'
+        >
+          <div className='relative h-96'>
+            <video
+              className='w-full brightness-[70%] object-cover h-full'
+              src={movie.videoUrl}
+              poster={movie.thumbnailUrl}
+              autoPlay
+              loop
+              muted
+            />
+            <div className='absolute bottom-[10%] left-10'>
+              <div className='flex flex-row gap-4 items-center'>
+                <PlayButton movieId={movie.id} />
+                <FavoriteButton movieId={movie.id} />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='p-8'>
-        <p className='text-white text-2xl md:text-3xl lg:text-4xl h-full font-bold mb-2'>
-          {movie.title}
-        </p>
-        <p className='text-white'>{movie.description}</p>
-        <hr className='bg-neutral-200 border-0 h-px my-4' />
-        <p className='text-white text-lg pb-4'>
-          Info on
-          <span className='font-bold'> {movie.title}</span>
-        </p>
-        <div className='flex flex-col gap-y-1'>
-          <p className='text-neutral-500 text-sm'>
-            Genre:
-            <span className='text-white'> {movie.genre}</span>
-          </p>
-          <p className='text-neutral-500 text-sm'>
-            Duration:
-            <span className='text-white'> {movie.duration}</span>
-          </p>
-        </div>
-      </div>
-    </Modal>
+          <div className='p-8 bg-zinc-900'>
+            <p className='text-white text-2xl md:text-3xl lg:text-4xl h-full font-bold mb-2'>
+              {movie.title}
+            </p>
+            <p className='text-white'>{movie.description}</p>
+            <hr className='bg-neutral-200 border-0 h-px my-4' />
+            <p className='text-white text-lg pb-4'>
+              Info on
+              <span className='font-bold'> {movie.title}</span>
+            </p>
+            <div className='flex flex-col gap-y-1'>
+              <p className='text-neutral-500 text-sm'>
+                Genre:
+                <span className='text-white'> {movie.genre}</span>
+              </p>
+              <p className='text-neutral-500 text-sm'>
+                Duration:
+                <span className='text-white'> {movie.duration}</span>
+              </p>
+            </div>
+          </div>
+
+          <Close asChild>
+            <button
+              className='absolute top-3 right-3 inline-flex h-10 w-10 rounded-full
+              bg-black appearance-none items-center justify-center focus:outline-none
+              border hover:border-opacity-70 border-white transition duration
+              group/item'
+              onClick={() => {}}
+            >
+              <IoMdClose
+                size={25}
+                className='text-white transition duration group-hover/item:text-opacity-70'
+              />
+            </button>
+          </Close>
+        </Content>
+      </Portal>
+    </Root>
   );
 };
 
--- components/Modals/MobileMenuModal.tsx
+++ components/Modals/MobileMenuModal.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import useMobileMenuModal from '@/hooks/useMobileMenuModal';
+import { Root, Portal, Overlay, Content } from '@radix-ui/react-dialog';
 
-import Modal from './Modal';
+import useMobileMenuModal from '@/hooks/useMobileMenuModal';
 
 const MobileMenuModal = () => {
   const { isOpen, onClose } = useMobileMenuModal();
@@ -14,11 +14,31 @@ const MobileMenuModal = () => {
   };
 
   return (
-    <Modal isOpen={isOpen} onChange={onChange} isCloseButton={false}>
-      <div className='w-full h-full flex flex-col gap-y-16 justify-center'>
-        <p className='text-white text-2xl'>Home</p>
-      </div>
-    </Modal>
+    <Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
+      <Portal>
+        <Overlay className='bg-neutral-900/90 backdrop-blur fixed inset-0 z-30' />
+        <Content className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-40'>
+          <div
+            className='h-full flex flex-col gap-y-16 items-center'
+            onClick={() => onChange(false)}
+          >
+            <p className='text-white text-xl shadow-xl cursor-pointer'>Home</p>
+            <p className='text-white text-xl shadow-xl cursor-pointer'>
+              TV Series
+            </p>
+            <p className='text-white text-xl shadow-xl cursor-pointer'>
+              Movies
+            </p>
+            <p className='text-white text-xl shadow-xl cursor-pointer'>
+              New & Popular
+            </p>
+            <p className='text-white text-xl shadow-xl cursor-pointer'>
+              My List
+            </p>
+          </div>
+        </Content>
+      </Portal>
+    </Root>
   );
 };
 

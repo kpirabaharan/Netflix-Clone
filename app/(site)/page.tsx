d11884e672e3fe5d1effbbe78c229cafae5956a9
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-16 15:13:31 -0400

    Added Pagination Arrows
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { redirect } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 
 import { Movie } from '@/types';
 import useMovieList from '@/hooks/useMovieList';
+import useMediaQuery from '@/hooks/useMediaQuery';
 
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
@@ -19,16 +21,51 @@ const HomePage = () => {
     },
   });
 
-  const { movies }: { movies: Movie[] } = useMovieList();
-  const { favorites }: { favorites: Movie[] } = useFavorites();
+  const isSmallScreens = useMediaQuery('(max-width: 767px)');
+  const isMediumScreens = useMediaQuery('(max-width: 1023px)');
+  const isLargeScreens = useMediaQuery('(max-width: 1535px)');
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [numberOfMovies, setNumberOfMovies] = useState(6);
+
+  // When MediaQueries Changes
+  useEffect(() => {
+    if (isSmallScreens) {
+      setNumberOfMovies(2);
+    } else if (isMediumScreens && !isSmallScreens) {
+      setNumberOfMovies(3);
+    } else if (isLargeScreens && !isMediumScreens && !isSmallScreens) {
+      setNumberOfMovies(4);
+    } else {
+      setNumberOfMovies(6);
+    }
+    // console.log(numberOfMovies);
+  }, [isLargeScreens, isMediumScreens, isSmallScreens, numberOfMovies]);
+
+  const { movies, count }: { movies: Movie[]; count: number } = useMovieList();
+  const {
+    favorites,
+    favoriteCount,
+  }: { favorites: Movie[]; favoriteCount: number } = useFavorites();
 
   return (
     <div>
       <Navbar />
       <Billboard />
       <div className='pb-40 flex flex-col gap-y-12 shadow-inner-upper-2xl pt-4'>
-        <MovieList title='Trending Now' movies={movies} />
-        <MovieList title='My List' movies={favorites} />
+        <MovieList
+          title='Trending Now'
+          movies={movies}
+          count={count}
+          rowSize={numberOfMovies}
+        />
+
+        <MovieList
+          title='My List'
+          movies={favorites}
+          count={favoriteCount}
+          rowSize={numberOfMovies}
+        />
       </div>
     </div>
   );
--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { isEmpty } from 'lodash';
+import { useState, useEffect } from 'react';
+import { isEmpty, round } from 'lodash';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 import { Movie } from '@/types';
@@ -10,9 +11,32 @@ import MovieCard from './MovieCard';
 interface MovieListProps {
   movies: Movie[];
   title: string;
+  count: number;
+  rowSize: number;
 }
 
-const MovieList = ({ title, movies }: MovieListProps) => {
+const MovieList = ({ title, movies, count, rowSize }: MovieListProps) => {
+  const [numberOfRows, setNumberOfRows] = useState(1);
+  const [row, setRow] = useState(0);
+
+  useEffect(() => {
+    setNumberOfRows(Math.ceil(count / rowSize));
+  }, [count, rowSize]);
+
+  const incrementRow = () => {
+    setRow((val) => val + 1);
+  };
+
+  const decrementRow = () => {
+    setRow((val) => val - 1);
+  };
+
+  const isDisabledLeft = row === 0;
+  const isDisabledRight = row === numberOfRows - 1;
+
+  const disableLeftButton = isDisabledLeft ? 'text-gray-700' : 'text-white';
+  const disableRightButton = isDisabledRight ? 'text-gray-700' : 'text-white';
+
   if (isEmpty(movies)) {
     return null;
   }
@@ -35,27 +59,29 @@ const MovieList = ({ title, movies }: MovieListProps) => {
 
         {/* Left Arrow */}
         <div
+          onClick={!isDisabledLeft ? () => decrementRow() : () => {}}
           className='h-[25vw] md:h-[18vw] lg:h-[13vw] 2xl:h-[9vw] w-12 
           flex justify-center items-center group/item cursor-pointer
           hover:bg-black/20 transition duration-300 absolute -left-12 top-0 z-20'
         >
           <FaChevronLeft
             size={40}
-            className='text-white opacity-0 group-hover/item:opacity-100
-            transition duration shadow-lg'
+            className={`opacity-0 group-hover/item:opacity-100
+            transition duration shadow-lg ${disableLeftButton}`}
           />
         </div>
 
         {/* Right Arrow */}
         <div
+          onClick={!isDisabledRight ? () => incrementRow() : () => {}}
           className='h-[25vw] md:h-[18vw] lg:h-[13vw] 2xl:h-[9vw] w-12  
           flex justify-center items-center group/item cursor-pointer
           hover:bg-black/20 transition duration-300 absolute -right-12 top-0 z-20'
         >
           <FaChevronRight
             size={40}
-            className='text-white opacity-0 group-hover/item:opacity-100
-            transition duration shadow-lg'
+            className={`opacity-0 group-hover/item:opacity-100
+            transition duration shadow-lg ${disableRightButton}`}
           />
         </div>
       </div>

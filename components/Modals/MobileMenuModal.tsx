9ff26db334d81204b595c632fcefd37d1864ade6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-16 20:52:20 -0400

    Spaced Out Menu Options Better
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- components/Modals/MobileMenuModal.tsx
+++ components/Modals/MobileMenuModal.tsx
@@ -17,22 +17,27 @@ const MobileMenuModal = () => {
     <Root open={isOpen} defaultOpen={isOpen} onOpenChange={onChange}>
       <Portal>
         <Overlay className='bg-neutral-900/90 backdrop-blur fixed inset-0 z-30' />
-        <Content className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-40 border-none'>
+        <Content
+          className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
+          z-40 border-none h-[80%]'
+        >
           <div
-            className='h-full flex flex-col gap-y-16 items-center'
+            className='h-full flex flex-col items-center justify-evenly'
             onClick={() => onChange(false)}
           >
-            <p className='text-white text-xl shadow-xl cursor-pointer'>Home</p>
-            <p className='text-white text-xl shadow-xl cursor-pointer'>
+            <p className='text-white text-sm sm:text-xl shadow-xl cursor-pointer'>
+              Home
+            </p>
+            <p className='text-white text-sm sm:text-xl shadow-xl cursor-pointer'>
               TV Series
             </p>
-            <p className='text-white text-xl shadow-xl cursor-pointer'>
+            <p className='text-white text-sm sm:text-xl shadow-xl cursor-pointer'>
               Movies
             </p>
-            <p className='text-white text-xl shadow-xl cursor-pointer'>
+            <p className='text-white text-sm sm:text-xl shadow-xl cursor-pointer'>
               New & Popular
             </p>
-            <p className='text-white text-xl shadow-xl cursor-pointer'>
+            <p className='text-white text-sm sm:text-xl shadow-xl cursor-pointer'>
               My List
             </p>
           </div>

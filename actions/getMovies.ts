48583a9769dabe47134ff25ab24fca327f6b608a
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-07-22 22:39:50 -0400

    Sort By New Movies
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- actions/getMovies.ts
+++ actions/getMovies.ts
@@ -1,6 +1,8 @@
 import prisma from '@/lib/prismadb';
 import getCurrentUser from '@/actions/getCurrentUser';
 
+import { sortBy } from 'lodash';
+
 const getMovies = async () => {
   try {
     const currentUser = await getCurrentUser();
@@ -10,9 +12,11 @@ const getMovies = async () => {
     }
 
     const movies = await prisma.movie.findMany();
+    const formattedMovies = sortBy(movies, 'createdAt').reverse();
+
     const count = await prisma.movie.count();
 
-    return { movies, count };
+    return { movies: formattedMovies, count };
   } catch (err: any) {
     console.log(err);
     return null;

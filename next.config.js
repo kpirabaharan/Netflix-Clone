556788ae030c8d0c02ba89f384ae3bb209dec585
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-07-02 21:13:35 -0400

    Updated S3 Bucket in Next Config
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['netflix-clone-kpirabaharan-images.s3.amazonaws.com'],
+    domains: ['netflix-clone-keeshigan-images.s3.amazonaws.com'],
   },
 };
 
